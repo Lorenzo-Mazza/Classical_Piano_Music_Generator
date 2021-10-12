47808d3348731ad4cf45f485be5e353465110e5c
Lorenzo Mazza <lorenzo2.mazza@mail.polimi.it>
2021-10-13 00:39:58 +0200

    adjust parameters try

--- GANtry.py
+++ GANtry.py
@@ -48,7 +48,7 @@ class MuseGAN:
 
         self.n_bars= FIXED_NUMBER_OF_BARS  # Generates only fixed length music
         self.n_steps_per_bar= quantization
-        self.weight_init = RandomNormal(mean=0., stddev=0.02)  # 'he_normal' #RandomNormal(mean=0., stddev=0.02)
+        self.weight_init ='he_normal' #RandomNormal(mean=0., stddev=0.02)
         self.batch_size = batch_size
 
         self.d_losses = []
@@ -59,23 +59,23 @@ class MuseGAN:
         """ Input Shape: N_SEQUENCES X SEQ_LENGTH for each feature (pitch,duration,velocity)"""
         """ Output Shape: N_SEQUENCES X N_DIFFERENT CLASSES for each feature (pitch,duration,velocity)"""
 
-        # Build and compile the discriminator
-        self.discriminator = self.build_discriminator()
+        # Build and compile the critic
+        self.critic = self.build_discriminator()
         # Build the generator
         self.generator = self.build_generator()
 
-        self.discriminator.compile(loss=self.wasserstein,
-            optimizer=optimizer,
-            metrics=['accuracy'])
+        self.critic.compile(loss=self.wasserstein,
+                            optimizer=optimizer,
+                            metrics=['accuracy'])
         # The generator takes noise as input and generated imgs
         z = Input(shape=(self.z_dim,))
         img = self.generator(z)
 
         # For the combined model we will only train the generator
-        self.discriminator.trainable = False
+        self.critic.trainable = False
 
         # The critic takes generated images as input and determines validity
-        valid = self.discriminator(img)
+        valid = self.critic(img)
 
         # The combined model  (stacked generator and critic)
         self.combined = Model(z, valid)
@@ -133,10 +133,9 @@ class MuseGAN:
         x = BatchNormalization(momentum=0.8)(x)
         x = Activation('relu')(x)
         x = Reshape([2, 1, 512])(x)
-        x = self.conv_t(x, f=512, k=(4, 1), s=(4, 1), a='relu', p='same', bn=True)
-        x = self.conv_t(x, f=256, k=(2, 1), s=(2, 1), a='relu', p='same', bn=True)
-        x = self.conv_t(x, f=256, k=(2, 1), s=(2, 1), a='relu', p='same', bn=True)
-        x = self.conv_t(x, f=128, k=(1, 8), s=(1, 8), a='relu', p='same', bn=True)
+        x = self.conv_t(x, f=128, k=(16, 1), s=(16, 1), a='relu', p='same', bn=True)
+        x = self.conv_t(x, f=64, k=(4, 1), s=(4, 1), a='relu', p='same', bn=True)
+        x = self.conv_t(x, f=32, k=(1, 2), s=(1, 2), a='relu', p='same', bn=True)
         x = self.conv_t(x, f=self.n_bars, k=(1, 16), s=(1, 16), a='tanh', p='same', bn=False)
         output_layer = Reshape([self.n_bars, self.n_steps_per_bar, self.input_shape,1])(x)
         return Model(input_layer, output_layer)
@@ -146,19 +145,18 @@ class MuseGAN:
         critic_input = Input(shape=(self.n_bars,self.n_steps_per_bar,self.input_shape,1), name='critic_input')
         x = critic_input
         x = self.conv(x, f=32, k=(2, 1, 1), s=(1, 1, 1), a='lrelu', p='valid')
+        x= (Dropout(0.25))(x)
         x = self.conv(x, f=64, k=(int(self.n_bars/2), 1, 1), s=(1, 1, 1), a='lrelu',p='valid')
         x= (Dropout(0.25))(x)
-        x = self.conv(x, f=128, k=(8, 1, 12), s=(8, 1, 12), a='lrelu', p='same')
-        x = self.conv(x, f=128, k=(1, 1, 7), s=(1, 1, 7), a='lrelu', p='same')
-        x = self.conv(x, f=128, k=(1, 2, 1), s=(1, 2, 1), a='lrelu', p='same')
-        x = self.conv(x, f=128, k=(1, 2, 1), s=(1, 2, 1), a='lrelu', p='same')
+        x = self.conv(x, f=128, k=(8, 1, 12*7), s=(8, 1, 12*7), a='lrelu', p='same')
         x= (Dropout(0.25))(x)
         x = self.conv(x, f=256, k=(1, 4, 1), s=(1, 2, 1), a='lrelu', p='same')
         x= (Dropout(0.25))(x)
         x = self.conv(x, f=512, k=(1, 3, 1), s=(1, 2, 1), a='lrelu', p='same')
+        x= (Dropout(0.25))(x)
         x = Flatten()(x)
-        x = Dense(1024, kernel_initializer=self.weight_init)(x)
-        x = LeakyReLU()(x)
+        #x = Dense(1024, kernel_initializer=self.weight_init)(x)
+        #x = LeakyReLU()(x)
         critic_output = Dense(1, activation=None, kernel_initializer=self.weight_init)(x)
 
         return Model(critic_input, critic_output)
@@ -192,9 +190,9 @@ class MuseGAN:
         noise_input = Input(shape=(self.z_dim,), name='noise_input')
         fake_img = self.generator(noise_input)
 
-        # discriminator determines validity of the real and fake images
-        fake = self.discriminator(fake_img)
-        valid = self.discriminator(real_img)
+        # critic determines validity of the real and fake images
+        fake = self.critic(fake_img)
+        valid = self.critic(real_img)
 
 
 
@@ -205,7 +203,7 @@ class MuseGAN:
             , optimizer=self.get_opti(self.discriminator_lr)
             , loss_weights=[1, 1])
         # For the generator we freeze the critic's layers
-        self.set_trainable(self.discriminator, False)
+        self.set_trainable(self.critic, False)
         self.set_trainable(self.generator, True)
 
         # Sampled noise for input to generator
@@ -214,14 +212,14 @@ class MuseGAN:
         # Generate images based of noise
         img = self.generator(noise_input)
         # Discriminator determines validity
-        model_output = self.discriminator(img)
+        model_output = self.critic(img)
         # Defines generator generator_model
         self.generator_model = Model(noise_input, model_output)
 
         self.generator_model.compile(optimizer=self.get_opti(self.generator_lr)
                                      , loss=self.wasserstein
                                      )
-        self.set_trainable(self.discriminator, True)
+        self.set_trainable(self.critic, True)
 
 
     def train_discriminator(self, x_train, batch_size, using_generator, x_train_iter):
@@ -286,12 +284,12 @@ class MuseGAN:
                 gen_imgs = self.generator.predict(noise)
 
                 # Train the critic
-                d_loss_real = self.discriminator.train_on_batch(imgs, valid)
-                d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
+                d_loss_real = self.critic.train_on_batch(imgs, valid)
+                d_loss_fake = self.critic.train_on_batch(gen_imgs, fake)
                 d_loss = 0.5 * np.add(d_loss_fake, d_loss_real)
 
                 # Clip critic weights
-                for l in self.discriminator.layers:
+                for l in self.critic.layers:
                     weights = l.get_weights()
                     weights = [np.clip(w, -self.clip_value, self.clip_value) for w in weights]
                     l.set_weights(weights)
@@ -317,7 +315,7 @@ class MuseGAN:
             for _ in range(critic_loops):
                 d_loss = self.train_discriminator(x_train, batch_size, using_generator, x_train_iter)
                 # Clip critic weights
-                for l in self.discriminator.layers:
+                for l in self.critic.layers:
                     weights = l.get_weights()
                     weights = [np.clip(w, -self.clip_value, self.clip_value) for w in weights]
                     l.set_weights(weights)
@@ -358,16 +356,16 @@ class MuseGAN:
             # Get the music sequences predicted by the generator
             gen_seq = self.generator.predict(noises)
 
-            # Train the discriminator (real classified as ones and generated as zeros)
-            d_loss_real = self.discriminator.train_on_batch(real_out, valid)
-            d_loss_fake = self.discriminator.train_on_batch(gen_seq, fake)
+            # Train the critic (real classified as ones and generated as zeros)
+            d_loss_real = self.critic.train_on_batch(real_out, valid)
+            d_loss_fake = self.critic.train_on_batch(gen_seq, fake)
             d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
             # ---------------------
             #  Train Generator
             # ---------------------
 
-            # Train the generator (wants discriminator to mistake his sequences as real)
+            # Train the generator (wants critic to mistake his sequences as real)
             g_loss = self.generator.train_on_batch(noises, real_out)
 
             # g_loss = self.combined.train_on_batch(noises, valid)
@@ -388,17 +386,17 @@ training_data = LoadPianoroll.load_data(fixed_timesteps)
 input_shape= training_data[0].shape[2]  # notes= 128
 training_data=LoadPianoroll.create_batches(training_data,BATCH_SIZE)
 # try 0.001 next
-optimizer= RMSprop(learning_rate=0.01)
+optimizer= RMSprop(learning_rate=0.0005)
 gan = MuseGAN(input_shape=training_data.element_spec.shape[3], discriminator_lr=0.00005
               , generator_lr=0.00005, optimiser=optimizer, z_dim=latent_dimension
               , batch_size=BATCH_SIZE, quantization=QUANTIZATION)
 gan.generator.summary()
-gan.discriminator.summary()
+gan.critic.summary()
 
-EPOCHS = 3000
+EPOCHS = 30
 PRINT_EVERY_N_BATCHES = 10
 gan.epoch = 0
-THRESHOLD= 0.9
+THRESHOLD= 0.75
 
 gan.train(
     training_data
@@ -410,6 +408,7 @@ gen_scores = gan.generator.predict(pred_noise)
 gen_scores = np.squeeze(gen_scores)
 #gen_scores = (gen_scores.astype(np.float32)*67.5) + 67.5
 gen_scores= np.reshape(gen_scores,(gen_scores.shape[0]*gen_scores.shape[1],-1))
+THRESHOLD*=np.max(gen_scores)
 gen_scores=np.where(gen_scores>THRESHOLD,70,0)
 track= pypianoroll.StandardTrack(pianoroll=gen_scores)
 #track=track.binarize()
