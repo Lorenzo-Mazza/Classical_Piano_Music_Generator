f55e8c7b706490ab596be7b3c1961ada64c6fb5d
Lorenzo Mazza <lorenzo2.mazza@mail.polimi.it>
2021-10-28 13:40:04 +0200

    run 11

--- GAN.py
+++ GAN.py
@@ -30,7 +30,7 @@ FIXED_NUMBER_OF_BARS= 8  # Baseline= 8
 FIXED_NUMBER_OF_QUARTERS= 4*FIXED_NUMBER_OF_BARS
 QUANTIZATION = 8
 BATCH_SIZE = 64  # Baseline= 64
-RUN_ID = '0010'
+RUN_ID = '0011'
 SECTION = 'compose'
 PARENT_FOLDER= os.getcwd()
 RUN_FOLDER = 'run/{}/'.format(SECTION)
@@ -244,7 +244,7 @@ class MuseGAN:
             d_losses.append(d_loss[0])
             # Plot the progress
             print("%d [D loss: %f] [G loss: %f]" % (epoch, d_loss[0], g_loss[0]))
-            if epoch % 500==0:
+            if epoch>250 and epoch % 125==0:
                 self.generator.save_weights(os.path.join(RUN_FOLDER, 'weights/weights-g-%d.h5' % epoch))
                 self.write_to_midi(epoch, run_folder=RUN_FOLDER)
 
@@ -276,7 +276,7 @@ print ("quantization is %d"%QUANTIZATION)
 training_data = LoadPianoroll.load_data(fixed_timesteps)
 input_shape= training_data[0].shape[2]  # notes= 128
 training_data=LoadPianoroll.create_batches(training_data,BATCH_SIZE)
-optimizer= RMSprop(learning_rate=0.0001)  # baseline=0.00005
+optimizer= RMSprop(learning_rate=0.001)  # baseline=0.00005
 gan = MuseGAN(input_shape=training_data.element_spec.shape[3], optimiser=optimizer, z_dim=latent_dimension
               , batch_size=BATCH_SIZE, quantization=QUANTIZATION)
 gan.generator.summary()
